8715d9dac3570577388cb63dc7b04f0064166e38
Anatolii Kochurkov <kochurkov@gmail.com>
2020-12-17 15:14:13 +0200

    start

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import {sum} from "./sum";
+
+const a = 5;
+const b = 10;
+
+console.log("Hello", a*a);
+console.log("World", b*a);
+console.log("sum =", sum(a, b));
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    //publicPath: './dist'
+    publicPath: './dist'
+  },
+  devServer: {
+    //contentBase: path.resolve(__dirname, 'dist'),
+    port: 9000,
+    contentBase: './',
+    publicPath: '/dist/'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
